d4231ec888aee3c7d33cbb2a4940a4b8072285b0
Jess James <jess.james2015@yahoo.com>
2021-02-08 10:50:52 -0600

    Starting to Configure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, {Component} from 'react';
+import {Navbar, NavbarBrand } from 'reractstrap';
 import logo from './logo.svg';
 import './App.css';
 
+// add Navbar here
 function App() {
   return (
     <div className="App">
